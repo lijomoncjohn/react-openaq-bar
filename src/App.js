8f2837bfb8f732bdfc3ab1c77781abe93d727a79
Lijomon C John <lijomon@promasoft.net>
2021-08-01 22:05:57 +0530

    Set layout for the app

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
 import './App.css';
+import About from './container/About';
+import Home from './container/Home';
+import Repo from './container/Repo';
+
 import Layout from './layout/Layout';
 
 function App() {
-	return <Layout />;
+	return (
+		<Router>
+			<Switch>
+				<Layout>
+					<Route path='/repo' component={Repo} />
+					<Route path='/about' component={About} />
+					<Route path='/' component={Home} />
+				</Layout>
+			</Switch>
+		</Router>
+	);
 }
 
 export default App;
--- src/layout/Layout.js
+++ src/layout/Layout.js
@@ -5,7 +5,7 @@ const Layout = ({ children }) => {
 	return (
 		<>
 			<NavBar />
-			{children}
+			<div className={'container'}>{children}</div>
 		</>
 	);
 };
